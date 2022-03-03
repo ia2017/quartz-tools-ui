4c7e8d11c989d30a60860eedc7a16ccc30b8c7c0
0xBriz <98397602+0xBriz@users.noreply.github.com>
2022-03-03 16:43:42 -0500

    Update price-tokens.ts

--- src/lib/data/price-tokens.ts
+++ src/lib/data/price-tokens.ts
@@ -1,7 +1,9 @@
 import { TokenPriceInfo } from '../types/token.types';
 import { BSC_PRICE_TOKENS } from './bsc/price-tokens';
-import { BINANCE_SMART_CHAIN } from './chains';
+import { BINANCE_SMART_CHAIN, HARMONY_CHAIN } from './chains';
+import { HARMONY_PRICE_TOKENS } from './harmony/price-tokens';
 
 export const PRICE_TOKENS: { [chainId: number]: TokenPriceInfo[] } = {
   [BINANCE_SMART_CHAIN.chainId]: BSC_PRICE_TOKENS,
+  [HARMONY_CHAIN.chainId]: HARMONY_PRICE_TOKENS,
 };
